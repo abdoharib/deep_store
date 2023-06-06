c6144dbb6ef787d2bb99f3c9de5e0913fcce664c
abdoharib <abdoamazon9@gmail.com>
2023-06-06 19:56:51 +0200

    7787878safsafsaf

--- app/actions/getRunningAdsAction.php
+++ app/actions/getRunningAdsAction.php
@@ -32,8 +32,10 @@ class getRunningAdsAction
             $json_date = explode('{',$item['name']);
             if(count($json_date)>1){
                 $json_date = $json_date[1];
+
+                dd('{'+$json_date);
                 try{
-                    $json_date = json_decode('{'+$json_date[1],true);
+                    $json_date = json_decode('{'+$json_date,true);
 
                 } catch (\JsonException $exception) {
                     dd('asfasfas');
