36b4dfc47d5199d5a486e6297a6f0ebdb53407a6
abdoharib <abdoamazon9@gmail.com>
2023-07-10 16:02:41 +0200

    safasfas

--- app/actions/adsRiskMangement.php
+++ app/actions/adsRiskMangement.php
@@ -34,7 +34,7 @@ class adsRiskMangement
         $facebook = new \JoelButcher\Facebook\Facebook([
             'app_id' => env('FACEBOOK_APP_ID','193483383509873'),
             'app_secret' => env('FACEBOOK_APP_SECRET','a5819237862894e7c0871fb1953a2bff'),
-            'default_access_token' => env('ACCESS_TOKEN','EAACvZBNxYE3EBAMijHiLcdJqJA36UdfYQ7DzZAutj1ytPXwKLPeEaJxOXiysOqS95q8tP4adMCjM3GY0dkfkUQjmEldgtSzgcvPff4dGEiWZAZAUWy4fAejvyspFzCB2mYrp3PCe6ljRd24or6XGJR30GBrHf2Owv7jcHxkdJ2aJkiDZCby6q'),
+            'default_access_token' => env('ACCESS_TOKEN','EAACvZBNxYE3EBAKprFnsWTfViLWBmtDkLq3Jo3IWOOglNFd0ymudZBZAC3Dif08EljHLPLulm3J5vPGEaCaZCXf1BZAzmcuuZBB43C32x8UnSxWZBoh7heZAgcOeKSZCDQRIJZBSJ8Pwam1fFpIIYQnJwrqC87gq39ZAyDoRkXbmS6ZAUMAdoHJFdxn2'),
             'default_graph_version' => env('FACEBOOK_GRAPH_VERSION', 'v16.0'),
         ]);
 
