9d4c702c2347b2187150ef3acc9bed67c01a7e71
abdoharib <abdoamazon9@gmail.com>
2023-07-16 02:12:55 +0200

    sfafsafaf

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -59,7 +59,7 @@ class Ad extends Model
         if($type == 'completed'){
             return $query
             ->where('ad_ref_effective_status','ADSET_PAUSED')
-            ->where('end_date','<=',now()->toDateTimeString());
+            ->orWhere('end_date','<=',now()->toDateTimeString());
 
         }
 
