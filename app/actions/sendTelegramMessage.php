50d91f1c7735bc87f174420019452dd921b05fd0
abdoharib <abdoamazon9@gmail.com>
2023-07-17 16:49:02 +0200

    afasfa

--- app/actions/sendTelegramMessage.php
+++ app/actions/sendTelegramMessage.php
@@ -12,7 +12,9 @@ use Symfony\Component\ErrorHandler\Debug;
 class sendTelegramMessage
 {
     public function invoke(string $message =''){
-        dd($message);
+        // dd($message);
+        Log::debug($message);
+
 
         try {
             $response = Http::post('https://api.telegram.org/bot6107962869:AAEnLYUlxM5Xqn4LqZ14nXzodkx7oZf8Q6A/sendMessage',[
