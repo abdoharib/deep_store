03986f0b637a9931e04d7c4ed41a70f5d5951cbd
abdoharib <abdoamazon9@gmail.com>
2023-07-24 14:57:03 +0200

    asfasf

--- app/actions/DailyRiskMangement.php
+++ app/actions/DailyRiskMangement.php
@@ -20,33 +20,33 @@ class DailyRiskMangement
     public function invoke()
     {
 
+        Log::debug(SupportCarbon::now()->toDateString());
+        // $NoSales = Sale::where('deleted_at', null)
+        //     ->where('date', '=', Carbon::now()->toDateString())
+        //     ->count();
+        // Log::debug('No of Sales Today : ' . $NoSales);
 
-        $NoSales = Sale::where('deleted_at', null)
-            ->where('date', '=', Carbon::now()->toDateString())
-            ->count();
-        Log::debug('No of Sales Today : ' . $NoSales);
+        // $amountSpent = Ad::ofType('active')
+        //     ->where('date', '=', Carbon::now()->toDateString())
+        //     ->where('deleted_at', '=', null)
+        //     ->get()
+        //     ->sum('amount_spent');
+        // Log::debug('Amount Spent of All Active Ads Today : ' . $amountSpent);
 
-        $amountSpent = Ad::ofType('active')
-            ->where('date', '=', Carbon::now()->toDateString())
-            ->where('deleted_at', '=', null)
-            ->get()
-            ->sum('amount_spent');
-        Log::debug('Amount Spent of All Active Ads Today : ' . $amountSpent);
+        // $cost_per_sale = ($amountSpent / $NoSales);
+        // $is_cost_per_sale_grater_then_10 =  $cost_per_sale > 10;
 
-        $cost_per_sale = ($amountSpent / $NoSales);
-        $is_cost_per_sale_grater_then_10 =  $cost_per_sale > 10;
+        // if ($is_cost_per_sale_grater_then_10) {
 
-        if ($is_cost_per_sale_grater_then_10) {
-
-            $no_ads_turned_off = 0;
-            foreach (Ad::ofType('active')->where('deleted_at', '=', null)->get() as $activeAd) {
-                if ($activeAd->preformance_status == 'average') {
-                    $this->adsRiskMangement->turnOffAd($activeAd);
-                    Log::debug("successfully turned off ad " . $activeAd->ad_ref_id);
-                    $no_ads_turned_off++;
-                }
-            }
-            Log::debug("Closed " . $no_ads_turned_off . ' average ads , daily cpr > 10');
-        }
+        //     $no_ads_turned_off = 0;
+        //     foreach (Ad::ofType('active')->where('deleted_at', '=', null)->get() as $activeAd) {
+        //         if ($activeAd->preformance_status == 'average') {
+        //             $this->adsRiskMangement->turnOffAd($activeAd);
+        //             Log::debug("successfully turned off ad " . $activeAd->ad_ref_id);
+        //             $no_ads_turned_off++;
+        //         }
+        //     }
+        //     Log::debug("Closed " . $no_ads_turned_off . ' average ads , daily cpr > 10');
+        // }
     }
 }
