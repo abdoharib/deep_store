d2510d6114664c67359a6aae14a3605d9ab0cad2
abdoharib <abdoamazon9@gmail.com>
2023-08-03 19:51:49 +0200

    af

--- app/actions/updateAdsAction.php
+++ app/actions/updateAdsAction.php
@@ -116,7 +116,10 @@ class updateAdsAction
                 $stop_time = $ad_data['campaign']['stop_time'];
             }
 
-
+            if($ad->ad_ref_id ==  '23856728194010392'){
+                Log::debug(SupportCarbon::make($ad_data['adset']['start_time'])->toDateTimeString());
+                Log::debug($ad_data['adset']);
+            }
                 $ad->update([
 
                     'campaing_ref_id' => $ad_data['campaign']['id'],
