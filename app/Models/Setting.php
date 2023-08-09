caa1b182350e087d655dcd9176eba4a6d2702731
abdoharib <abdoamazon9@gmail.com>
2023-08-09 14:00:55 +0200

    asf

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -13,7 +13,8 @@ class Setting extends Model
     protected $fillable = [
         'currency_id', 'email', 'CompanyName', 'CompanyPhone', 'CompanyAdress',
          'logo','footer','developed_by','client_id','warehouse_id','default_language',
-         'is_invoice_footer','invoice_footer','vanex_api_key','facebook_user_token'
+         'is_invoice_footer','invoice_footer','vanex_api_key',
+         'facebook_user_token','facebook_app_id','facebook_app_secret'
     ];
 
     protected $casts = [
